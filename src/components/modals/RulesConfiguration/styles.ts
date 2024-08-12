cea4b4f8a9ae6c782f81bb97ae0f4b2d470becc6
Diego Pinilla <dpinilla@sistemasenlinea.com.co>
2024-08-12 14:13:16 -0500

    fix: adjustment to styles

--- src/components/modals/RulesConfiguration/styles.ts
+++ src/components/modals/RulesConfiguration/styles.ts
@@ -8,12 +8,10 @@ const StyledContainer = styled.div`
 const StyledModal = styled.div`
   background-color: ${({ theme }) =>
     theme?.palette?.neutral?.N10 || inube.palette.neutral.N10};
-
   width: 450px;
   min-height: auto;
   height: auto;
   border-radius: 8px;
-
   & > div {
     padding: 24px;
   }
