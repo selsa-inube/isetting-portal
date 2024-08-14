cdac15514c65699c386f68fe3ac388240a11514d
Diego Pinilla <dpinilla@sistemasenlinea.com.co>
2024-08-14 16:03:57 -0500

    fix: remove path

--- src/routes/rules.tsx
+++ src/routes/rules.tsx
@@ -2,12 +2,10 @@ import { Route, Routes } from "react-router-dom";
 
 import { ErrorPage } from "@components/layout/ErrorPage";
 import { RulesOptions } from "@src/pages/rules/outlets/options";
-import { Rules } from "@src/pages/rules";
 
 function RulesRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Rules />} />
       <Route path="/options" element={<RulesOptions />} />
       <Route path="/*" element={<ErrorPage />} />
     </Routes>
