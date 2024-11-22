39b4aa74a99c86fd5105247d405e6eeaaced527c
LinconOssa <lossa@sistemasenlinea.com.co>
2024-11-22 09:29:52 -0500

    fix: authentication

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { environment } from "@config/environment.js";
+import { App } from "./App.js";
 
+const redirect_uri = window.location.origin;
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-          <App />
+    <Auth0Provider
+      domain={environment.AUTH0_DOMAIN}
+      clientId={environment.CLIENT_ID}
+      authorizationParams={{
+        redirect_uri,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
