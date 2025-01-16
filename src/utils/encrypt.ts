742bb2f51acbbb8f5c198eb26ae9c6e3756490ef
LinconOssa <lossa@sistemasenlinea.com.co>
2025-01-16 10:58:50 -0500

    fix: rename ioptionsByStaffPortalBusinessManage

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,5 +1,5 @@
-import { secretKeyPortalId } from "@config/environment";
 import CryptoJS from "crypto-js";
+import { secretKeyPortalId } from "@config/environment";
 const secretKey = CryptoJS.enc.Hex.parse(secretKeyPortalId);
 const iv = CryptoJS.enc.Hex.parse("abcdef9876543210abcdef9876543210");
 
